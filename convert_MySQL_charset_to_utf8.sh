a43f41c7deb77b680b7b7df6c46ec0d1a4848f78
Ox0spy <Ox0spy@gmail.com>
2013-06-03 14:10:31 +0800

    add a script: convert MySQL charset to UTF8.

---
+++ convert_MySQL_charset_to_utf8.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ox0spy <ox0spy[at]gmail.com>
+
+# get MySQL root password
+read -s -p "Please input MySQL root password: " pw
+echo
+
+mysql_cmd="mysql -u root -p$pw"
+all_db='clid bigbrother'
+# process database and all table in those databases
+for db in $all_db
+do
+    echo "alter database $db charset = utf8" | $mysql_cmd
+    mysql_cmd_with_db="$mysql_cmd -D $db"
+    for x in $(echo 'show tables' | $mysql_cmd_with_db | \
+        grep -Ev "^Tables_in_$db")
+        do
+            echo $x
+            sql="ALTER TABLE $x CONVERT TO CHARACTER SET utf8"
+            echo $sql | $mysql_cmd_with_db
+            echo "done, convert $x to utf8"
+        done
+done
+
+# change mysqld and mysql client charset
+# change /etc/mysql/my.cnf, add "default-character-set=utf8" under [mysqld] and [mysql]
+# then, restart mysqld: $ sudo services mysql restart
